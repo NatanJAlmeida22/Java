3d47fa04682bf1f1c6111a16028a52e68b23993c
Natan José de Almeida <natanalmeida158@hotmail.com>
2021-08-10 13:49:24 -0300

    Criação do main, classe Pessoa e Livro

---
+++ POO/ProjetoLivro/src/projetolivro/Livro.java
@@ -0,0 +1,59 @@
+package projetolivro;
+public class Livro {
+    private String livro, autor;
+    private int totPaginas, pagAtual;
+    private boolean aberto;
+    private Pessoa leitor;
+
+    public String getLivro() {
+        return livro;
+    }
+
+    public void setLivro(String livro) {
+        this.livro = livro;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getTotPaginas() {
+        return totPaginas;
+    }
+
+    public void setTotPaginas(int totPaginas) {
+        this.totPaginas = totPaginas;
+    }
+
+    public int getPagAtual() {
+        return pagAtual;
+    }
+
+    public void setPagAtual(int pagAtual) {
+        this.pagAtual = pagAtual;
+    }
+
+    public boolean isAberto() {
+        return aberto;
+    }
+
+    public void setAberto(boolean aberto) {
+        this.aberto = aberto;
+    }
+
+    public Pessoa getLeitor() {
+        return leitor;
+    }
+
+    public void setLeitor(Pessoa leitor) {
+        this.leitor = leitor;
+    }
+    
+    public void detallhes(){
+        
+    }
+}
---
+++ POO/ProjetoLivro/src/projetolivro/ProjetoLivro.java
@@ -0,0 +1,7 @@
+package projetolivro;
+public class ProjetoLivro {
+    public static void main(String[] args) {
+        
+    }
+    
+}
