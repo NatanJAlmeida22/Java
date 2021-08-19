90c67f8ecc12256e44eb005d0d624820640e3040
Natan José de Almeida <natanalmeida158@hotmail.com>
2021-08-19 17:09:33 -0300

    Criação do main e a interface

---
+++ POO/ProjetoYouTube/src/projetoyoutube/AcoesVideo.java
@@ -0,0 +1,4 @@
+package projetoyoutube;
+public interface AcoesVideo {
+    
+}
---
+++ POO/ProjetoYouTube/src/projetoyoutube/ProjetoYouTube.java
@@ -0,0 +1,7 @@
+package projetoyoutube;
+public class ProjetoYouTube {
+    public static void main(String[] args) {
+        
+    }
+    
+}
