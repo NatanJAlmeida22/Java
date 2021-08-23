91d9f562f9717ff0a0d30191b2fa94978ee28332
Natan José de Almeida <natanalmeida158@hotmail.com>
2021-08-23 09:46:01 -0300

    Nova Pasta e projeto sobre List

---
+++ Collections-e-Streams/ExemploList/src/exemplolist/ExemploList.java
@@ -0,0 +1,19 @@
+package exemplolist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExemploList {
+    public static void main(String[] args) {
+        List<String> nomes = new ArrayList<>();
+        
+        nomes.add("Carlos");
+        nomes.add("Pedro");
+        nomes.add("Juliana");
+        nomes.add("Maria");
+        nomes.add("João");
+        
+        System.out.println(nomes);
+    }
+    
+}
